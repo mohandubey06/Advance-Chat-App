3242bfabbb920eaad5a93083c49d9af6773254a2
9064739843 <mohandubey06@gmail.com>
2025-03-07 22:38:08 +0530

    first commit

---
+++ server/routes/AuthRoutes.js
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import {
+  getUserInfo,
+  login,
+  signup,
+  logout,
+  updateProfile,
+  addProfileImage,
+  removeProfileImage,
+ 
+} from "../controllers/AuthController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
+import multer from "multer";
+
+
+const authRoutes = Router();
+const upload = multer({ dest: "uploads/profiles/" });
+
+authRoutes.post("/signup", signup);
+authRoutes.post("/login", login);
+authRoutes.post("/logout", logout);
+authRoutes.get("/userinfo", verifyToken, getUserInfo);
+authRoutes.post("/update-profile", verifyToken, updateProfile);
+authRoutes.post(
+  "/add-profile-image",
+  verifyToken,
+  upload.single("profile-image"),
+  addProfileImage
+);
+authRoutes.get("/onlyinfo", verifyToken);
+
+
+authRoutes.delete("/remove-profile-image", verifyToken, removeProfileImage);
+
+authRoutes.get("/current_user",verifyToken)
+
+export default authRoutes;
